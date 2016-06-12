29b982dff220cb70cc72e541da4f51560520c027
Anrushenko Alexander <fakee-mail@inbox.ru>
2016-06-12 12:46:01 +0700

    Попытка поля

---
+++ Assets/Map.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+
+    public Transform tile;
+    
+    public int xTiles;
+    public int yTiles;
+
+    void Start()
+    {
+        for (int y = 0; y < yTiles; y++)
+        {
+            for (int x = 0; x < xTiles; x++)
+            {
+                float rnd = Random.value;
+                if (rnd < 0.05)
+                {
+                    Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}

cc6f7892d4dcfba768c42fd3e7167d091b585e51
Anrushenko Alexander <fakee-mail@inbox.ru>
2016-06-14 01:02:38 +0700

    Ортокамера

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += move * 1 * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+
+    public GameObject tile;
+    
+    public int xTiles;
+    public int yTiles;
+    float X = 0;
+    float Y = 0;
+
+    void Start()
+    {
+        for (int y = 0; y < yTiles; y++)
+        {
+            for (int x = 0; x < xTiles; x++)
+            {
+                    Instantiate(tile, new Vector3(X, Y, 0), Quaternion.identity);
+                X += tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            }
+            Y += tile.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+            X = 0;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
